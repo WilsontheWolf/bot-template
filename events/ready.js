3928941130e6f34b0cef50a0e0bdfb66fcdb68ea
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2020-09-06 02:05:21 -0600

    status

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,7 @@ module.exports = async (client) => {
 	console.log(`Connected as ${client.user.tag} (${client.user.id})`)
 	console.log(`I'm in ${client.guilds.cache.size} guilds, with ${client.channels.cache.size} channels and ${client.users.cache.size} users.`)
 
+	client.user.setActivity(`for @${client.user.username} help`, { type: 'WATCHING' })
 	// ensure the internal db has the important stuff
 	client.internal.ensure('support', [])
 }
